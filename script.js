0bd782ceae320327cb89435cc43882f3a2748b7a
Rafayet07 <refayet0096@gmail.com>
2024-03-25 02:23:30 +0600

    initializing

---
+++ script.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!')
+});
+
+
+app.listen(5000);
