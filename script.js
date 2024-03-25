8b1771eda6a429e6f0e9c3de37ddbfe7d3ec7f35
refayet-dev <61475826+refayet-dev@users.noreply.github.com>
2024-03-25 20:56:33 +0600

    add

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ env.config();
 const app = express();
 app.use(express.json());
 
-
+app.use(express.static('public'));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
